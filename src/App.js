b8d1a05a4a6775d15c87f67100e472b0a4a6fe73
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-10-09 17:38:20 +0200

    update trying to make things worrk..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Carousel from "./components/Carousel";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <>
+      <Navigation />
+      <Carousel />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function renderNavigation() {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default renderNavigation;
