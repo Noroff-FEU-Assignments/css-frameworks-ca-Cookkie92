0955357af5a3f8ab274f26721d19f00296c6c317
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-10-09 20:49:52 +0200

    trying router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
-import Carousel from "./components/Carousel";
+import Carousel from "./components/home/Carousel";
+import Tabs from "./components/home/Tabs";
 
 function App() {
   return (
-    <>
-      <Navigation />
-      <Carousel />
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <News />
+          </Route>
+          <Route path="/">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import Navigation from "./components/Navigation";
+import Carousel from "./components/home/Carousel";
+import Tabs from "./components/home/Tabs";
+
+function Home() {
+  return (
+    <>
+      <Navigation />
+      <Carousel />
+      <Tabs />
+    </>
+  );
+}
+
+export default Home;
